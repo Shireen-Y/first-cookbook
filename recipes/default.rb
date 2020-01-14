fad1a3ab1d36fa724f2b7e5c5541a80cc675258f
Shireen Yawer <SYawer@spartaglobal.com>
2020-01-14 15:40:45 +0000

    first cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,19 @@
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
+
+# Imports of recipes
+include_recipe 'nodejs'
+include_recipe 'apt'
+
+# Packages apt-get
+apt_update
+package 'nginx'
+
+# services
+service 'nginx' do
+  action [ :enable, :start ]
+end
+
+# npm installs
+nodejs_npm 'pm2'
